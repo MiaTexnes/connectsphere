a2ad65556161e406f5841bcf2314d1063bbc9474
MiaTexnes <miatex50428@stud.noroff.no>
2024-11-24 11:24:01 +0100

    headings, favicon from js etc

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const btn = document.querySelector("#menu-btn");
-const menu = document.querySelector("#mobile-menu");
+function addFaviconsAndManifest() {
+  const head = document.head;
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
+  const links = [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/favicon/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/favicon/site.webmanifest" },
+  ];
+
+  links.forEach((linkInfo) => {
+    const link = document.createElement("link");
+    Object.keys(linkInfo).forEach((attr) =>
+      link.setAttribute(attr, linkInfo[attr])
+    );
+    head.appendChild(link);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", addFaviconsAndManifest);
